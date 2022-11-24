015c3a42f3e4193e51ab1810565f24695b198cb6
Star-mi <star-mi-7@mail.ru>
2022-11-24 14:49:16 +0200

    create Student

---
+++ src/test/java/com/makarohadev/fullstackcourse/FullStackCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makarohadev.fullstackcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
