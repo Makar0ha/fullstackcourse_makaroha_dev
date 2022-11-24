63d55b445eb65f7b969e11d36d113b3c7de7682d
Star-mi <star-mi-7@mail.ru>
2022-11-24 18:02:15 +0200

    create UI for front with ANTD

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
+import {getAllStudents} from "./client";
 import './App.css';
+import React, {useEffect, useState} from "react";
+import {Breadcrumb, Layout, Menu} from 'antd';
+import {
+    DesktopOutlined,
+    FileOutlined,
+    PieChartOutlined,
+    TeamOutlined,
+    UserOutlined,
+} from '@ant-design/icons';
+
+const {Header, Content, Footer, Sider} = Layout;
+
+function getItem(label, key, icon, children) {
+    return {
+        key,
+        icon,
+        children,
+        label,
+    };
+}
+
+const items = [
+    getItem('Option 1', '1', <PieChartOutlined />),
+    getItem('Option 2', '2', <DesktopOutlined />),
+    getItem('User', 'sub1', <UserOutlined />, [
+        getItem('Tom', '3'),
+        getItem('Bill', '4'),
+        getItem('Alex', '5'),
+    ]),
+    getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+    getItem('Files', '9', <FileOutlined />),
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [collapsed, setCollapsed] = useState(false);
+    const [students, setStudents] = useState([]);
+    const fetchStudents = () =>
+        getAllStudents()
+            .then(res => res.json())
+            .then(data => setStudents(data));
+
+    useEffect(() => {
+        console.log("comp moumt");
+        fetchStudents();
+    }, [])
+
+    if (students.length <= 0) {
+        return "no data";
+    }
+    return <Layout
+        style={{
+            minHeight: '100vh',
+        }}
+    >
+        <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+            <div className="logo"/>
+            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items}/>
+        </Sider>
+        <Layout className="site-layout">
+            <Header
+                className="site-layout-background"
+                style={{
+                    padding: 0,
+                }}
+            />
+            <Content
+                style={{
+                    margin: '0 16px',
+                }}
+            >
+                <Breadcrumb
+                    style={{
+                        margin: '16px 0',
+                    }}
+                >
+                    <Breadcrumb.Item>User</Breadcrumb.Item>
+                    <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                </Breadcrumb>
+                <div
+                    className="site-layout-background"
+                    style={{
+                        padding: 24,
+                        minHeight: 360,
+                    }}
+                >
+                    Bill is a cat.
+                </div>
+            </Content>
+            <Footer
+                style={{
+                    textAlign: 'center',
+                }}
+            >
+            </Footer>
+        </Layout>
+    </Layout>
 }
 
 export default App;
