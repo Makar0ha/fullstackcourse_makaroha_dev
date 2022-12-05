00041acbe5f58f56cc8117b6370cedaf973191cc
Star-mi <star-mi-7@mail.ru>
2022-12-06 00:13:59 +0200

    make test pass

--- src/test/java/com/makarohadev/fullstackcourse/FullStackCourseApplicationTests.java
+++ src/test/java/com/makarohadev/fullstackcourse/FullStackCourseApplicationTests.java
@@ -9,7 +9,7 @@ class FullStackCourseApplicationTests {
 
     @Test
     void contextLoads() {
-        Assertions.fail("Ooops test failed");
+//        Assertions.fail("Ooops test failed");
     }
 
 }
